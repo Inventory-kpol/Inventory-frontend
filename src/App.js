a7b7d093c746c43403dd1873abf73b9c7e5e61aa
silverSani <eunsan12345@naver.com>
2024-11-27 13:59:31 +0900

    Frontend Init

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Route, Routes } from "react-router-dom";
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />}></Route>
     </Routes>
   );
 }
