71a0f13dd7c11652ef41cbf544f109c55ac62be2
silverSani <eunsan12345@naver.com>
2025-02-17 19:17:34 +0900

    [Chore]게시글

--- src/pages/Column/Column.jsx
+++ src/pages/Column/Column.jsx
@@ -4,10 +4,15 @@ import { useNavigate } from "react-router-dom";
 function Column(){
 
     const navigate = useNavigate();
-
+    
+    return (
+        <div>
+            
+        </div>
+    );
     
 
-    
+
 
 }
 
