a02b09233b1a09e327009ad4909ab970b8d78dff
Yuchanyeong <xkal1118@naver.com>
2025-03-04 16:35:46 +0900

    [Fix] 마이페이지, 정보 수정 페이지

--- src/pages/Mypage/Mypage_Setting.jsx
+++ src/pages/Mypage/Mypage_Setting.jsx
@@ -9,23 +9,36 @@ import { useNavigate } from "react-router-dom"; // useNavigate import
 
 const MyPage_Setting = () => {
  const navigate = useNavigate(); // 페이지 이동을 위한 useNavigate
+ const [user, setUser] = useState();
 
 
   return (
     <div>
       <Header />
       <div className="mypage-content">
-      <div className="profile-section">
+        <div className="profile-section">
             <img
               src="/default-profile.png"
-          
               className="profile-image"
             />
-           
-          {/* 프로필 수정 버튼 */}
-            <button onClick={() => navigate("/profile")}>이미지 업로드</button>
-          </div>
-    </div>
+            {/* 프로필 수정 버튼 */}
+            <button>이미지 업로드</button>
+        </div>
+        <div>
+          <p>이름</p>
+          <button>이름 수정</button>
+        </div>
+
+        <div> 
+          <p>닉네임</p>
+          <button>닉네임 수정</button>
+        </div>
+        <div> 
+          <p>비밀번호</p>
+          <button>비밀번호 수정</button>
+        </div>
+        
+      </div>
     </div>
   );
 };
